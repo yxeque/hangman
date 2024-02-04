fde8593b5b68557172253ab8dc2c2ac92b0ff087
yxeque <yxequel@gmail.com>
2024-02-04 11:18:13 +0800

    Add display_word method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -10,10 +10,21 @@ class Hangman
   end
 
   def load_dictionary
-  @dictionary = File.readlines("google-10000-english-no-swears.txt").map(&:chomp).select { |word| word.length.between?(5, 12) }
-
+    @dictionary = File.readlines("google-10000-english-no-swears.txt").map(&:chomp).select { |word| word.length.between?(5, 12) }
+  end
   def choose_secret_word
     @secret_word = @dictionary.sample
   end
-end
+
+  def display_word
+    displayed_word = ""
+    @secret_word.each_char do |char|
+      if @guessed_letters.include?(char)
+        displayed_word += char
+      else
+        displayed_word += "_"
+      end
+    end
+    return displayed_word
+  end
 end
